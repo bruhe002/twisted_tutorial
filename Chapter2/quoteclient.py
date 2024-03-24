1a1a126c7aeae26da84825240392270d962fecfa
Eric Bruhns <bruhe002@gmail.com>
2024-03-23 23:40:30 -0600

    Finished quoteclient

--- Chapter2/quoteclient.py
+++ Chapter2/quoteclient.py
@@ -14,3 +14,40 @@ class QuoteProtocol(protocol.Protocol):
         print("Received quote:", data)
         self.transport.loseConnection()
 
+class QuoteClientFactory(protocol.ClientFactory):
+    def __init__(self, quote):
+        self.quote = quote
+
+    def buildProtocol(self, addr):
+        return QuoteProtocol(self)
+    
+    def clientConnectionFailed(self, connector, reason):
+        print("connection failed:", reason.getErrorMessage())
+        maybeStopReactor()
+
+    def clientConnectionLost(self, connector, reason):
+        print("connection failed:", reason.getErrorMessage())
+        maybeStopReactor()
+
+def maybeStopReactor():
+    global quote_counter
+    quote_counter -= 1
+    if not quote_counter:
+        reactor.stop()
+
+quotes = [
+    "You snooze you lose",
+    "The Early Bird Gets the Worm",
+    "Carpe Diem"
+]
+
+quote_counter = len(quotes)
+
+for q in quotes:
+    reactor.connectTCP('localhost', 8000, QuoteClientFactory(q))
+
+reactor.run()
+
+
+
+    
