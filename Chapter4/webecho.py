7d8793fa45f65524a0a69b46dad4331eb094cc8c
Eric Bruhns <bruhe002@gmail.com>
2024-04-05 23:37:43 -0600

    Finish httpecho class

--- Chapter4/webecho.py
+++ Chapter4/webecho.py
@@ -16,3 +16,11 @@ class HTTPEchoProtocol(basic.LineReceiver):
         # If no lines left, send response
         if not line:
             self.sendResponse()
+
+    def sendResponse(self):
+        self.sendLine("HTTP/1.1 200 OK")
+        self.sendLine("")
+        responseBody = "You said:\r\n\r\n" + "\r\n".join(self.lines)
+        self.transport.write(responseBody)
+        self.transport.loseConnection()
+        
