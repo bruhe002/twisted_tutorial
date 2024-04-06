f39d051eef8c3873027a6065daab9439e53a5087
Eric Bruhns <bruhe002@gmail.com>
2024-04-06 15:06:32 -0600

    Trying the clock example

---
+++ Chapter4/clock_dynamic_content.py
@@ -0,0 +1,19 @@
+'''
+    Use of Site to access dynamic content
+'''
+
+from twisted.internet import reactor
+from twisted.web.resource import Resource
+from twisted.web.server import Site
+
+import time
+
+class ClockPage(Resource):
+    isLeaf = True
+    def render_GET(self, request):
+        return "The local time is %s" % (time.ctime(),)
+    
+resource = ClockPage()
+factory = Site(resource)
+reactor.listenTCP(8000, factory)
+reactor.run()
