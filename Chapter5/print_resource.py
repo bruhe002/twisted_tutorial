5af2e2d0dce94daac05a411baf9824ec71a5fb7f
Eric Bruhns <bruhe002@gmail.com>
2024-04-12 22:52:59 -0600

    Finished print resource file

---
+++ Chapter5/print_resource.py
@@ -0,0 +1,27 @@
+'''
+    Shows the advantages of getPage
+'''
+from twisted.internet import reactor
+from twisted.web.client import getPage
+import sys
+
+# callbacks for the deferred object
+def printPage(result):
+    print(result)
+
+def printError(failure):
+    print(sys.stderr, failure)
+
+def stop(result):
+    reactor.stop()
+
+if len(sys.argv) != 2:
+    print(sys.stderr, "Usage: python print_resource.py <URL>")
+    exit(1)
+
+# Get page returns a deferred object
+d = getPage(sys.argv[1])
+d.addCallbacks(printPage, printError)
+d.addBoth(stop)
+
+reactor.run()
